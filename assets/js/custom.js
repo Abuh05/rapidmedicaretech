22f70f8caf7a66ae5d2caf6380ec4d24e9381da4
Abuh Solomon <abuhsolo05@gmail.com>
2022-05-18 17:50:03 +0100

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -233,7 +233,7 @@
             disableOn: 100,
             type: 'iframe',
             mainClass: 'mfp-fade',
-            removalDelay: 160,
+            removalDelay: 200,
             preloader: false,
             fixedContentPos: false
         });
@@ -354,26 +354,7 @@
 	// $('body').append("<div class='switch-box'><label id='switch' class='switch'><input type='checkbox' onchange='toggleTheme()' id='slider'><span class='slider round'></span></label></div>");
     
 })(jQuery);
-// function to set a given theme/color-scheme
-// function setTheme(themeName) {
-//     localStorage.setItem('genh_theme', themeName);
-//     document.documentElement.className = themeName;
-// }
-// function to toggle between light and dark theme
-// function toggleTheme() {
-//     if (localStorage.getItem('genh_theme') === 'theme-dark') {
-//         setTheme('theme-light');
-//     } else {
-//         setTheme('theme-dark');
-//     }
-// }
-// Immediately invoked function to set the theme on initial load
-// (function () {
-//     if (localStorage.getItem('genh_theme') === 'theme-dark') {
-//         setTheme('theme-dark');
-//         document.getElementById('slider').checked = false;
-//     } else {
-//         setTheme('theme-light');
-//       document.getElementById('slider').checked = true;
-//     }
-// })();
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+});
